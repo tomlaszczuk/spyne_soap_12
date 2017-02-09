f7134ecc508a08bc0c1b1e944e3595029a96f956
Michał Urbański <m.urbanski@futuremind.com>
2017-02-09 12:21:03 +0100

    working soap example

--- app.py
+++ app.py
@@ -4,22 +4,33 @@ from spyne import ServiceBase
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
 from spyne.application import Application
-from spyne.protocol.soap import Soap11
+from spyne.protocol.soap import Soap11, Soap12
 from spyne.server.wsgi import WsgiApplication
 from spyne.decorator import rpc
 
 
+def on_method_return_string(ctx):
+    ctx.out_string[0] = ctx.out_string[0].replace(b'tns', b'targetNamespace')
+
+
 class HelloWorldService(ServiceBase):
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
+
+    @rpc(Unicode, Integer, _returns=Iterable(Unicode),
+         _in_message_name='create',
+         _out_message_name='created',
+         _out_variable_name='created')
     def say_hello(ctx, name, times):
         for i in range(times):
             yield 'Hello, %s' % name
 
 
+HelloWorldService.event_manager.add_listener('method_return_string',
+                                             on_method_return_string)
+
 application = Application([HelloWorldService],
-                          tns='spyne.examples.hello',
-                          in_protocol=Soap11(validator='lxml'),
-                          out_protocol=Soap11())
+                          tns='targetNamespace',
+                          in_protocol=Soap12(validator='lxml'),
+                          out_protocol=Soap12())
 
 
 if __name__ == '__main__':
@@ -27,7 +38,7 @@ if __name__ == '__main__':
     # Python's built-in wsgi server but you're not
     # supposed to use it in production.
     from wsgiref.simple_server import make_server
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
     wsgi_app = WsgiApplication(application)
     server = make_server('0.0.0.0', 8000, wsgi_app)
--- client.py
+++ client.py
@@ -1,6 +1,11 @@
+from suds.client import Client
+from suds.bindings import binding
+
+
 if __name__ == "__main__":
-    from suds.client import Client
-    hello_client = Client('http://localhost:8000/?wsdl')
-    print("tomek")
+    binding.envns = ('SOAP-ENV', 'http://www.w3.org/2003/05/soap-envelope')
+    hello_client = Client('http://localhost:8000/?wsdl', retxml=True)
+    print(hello_client)
     print(hello_client.service.say_hello("Dave", 5))
+    print(hello_client.last_received())
 
